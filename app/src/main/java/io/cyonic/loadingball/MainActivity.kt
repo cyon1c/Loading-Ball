fe79fc68d4390f4c40949f614f9bed60a72da8d7
elli <johnelliotwhite@gmail.com>
2019-12-03 20:31:12 -0600

    Initial Project

---
+++ app/src/main/java/io/cyonic/loadingball/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.cyonic.loadingball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
